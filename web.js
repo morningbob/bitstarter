14d3b1037a0d8546a971ffd66270326aadc70498
morningbob <hon898@gmail.com>
2013-07-05 19:27:10 +0000

    1-try part 1

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
+
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html');
+
+var buffer = new Buffer(index,"utf-8");
+
+var output = buffer.toString("utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
